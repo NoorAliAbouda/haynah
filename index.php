543e492d8c0e941a6bdbdbd2871badb4255321c0
NoorAliAbouda <141049586+NoorAliAbouda@users.noreply.github.com>
2023-09-27 14:40:41 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Haynah</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/normal.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="logo">
+                <img src="haynah no tm - Final/logo/logo.png" alt="">
+            </div>
+            <div class="btn">
+                <button>SIGNIN</button>
+                <button class="up">SIGNUP</button>
+            </div>
+        </header>
+    </div>
+
+    <section>
+        <div class="container sec">
+            <div class="text">
+                <h1> Welcome to Haynah Investment</h1>
+
+                <p> We aim to achieve a 10M $ market value and serve 700-1000 customers
+                    daily in Restaurant, Grocery, Parcel, and traditional B&M retailers as a SaaS solution.
+                    The launching investment total isa 15-20K $,
+                    and each 1000$ is for 0.75% stock share.</p>
+
+                <div class="btn2">
+                    <button>Email Us</button>
+                    <button class="call">Call Us</button>
+                </div>
+            </div>
+
+            <div class="form-details">
+                    <h3>To Invest</h3>
+
+                    <div class="alert">
+                        Please fill in the following fields
+                    </div>
+                    
+                    <div class="name f">
+                        <label for="name">Your full name</label>
+                        <input type="text" name="user" id="name" placeholder="Your full name">
+                    </div>
+                    <div class="price f">
+                        <label for="jod">Investment amount in JOD</label>
+                        <input type="number" name="price" id="jod" placeholder="Proceed to agreement">
+                    </div>
+
+                    <div class="submit">
+                        <input type="submit" name="submit" value="Proceed to agreement">
+                        <img src="fav Icon/Artboard 5 copy 2.png" alt="" srcset="">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        Copyright © 2023, Haynah, All Rights Reserved
+    </footer>
+
+
+    <script src="js/script.js"></script>
+</body>
+</html>
